f8e092a5e863d07caa3a9bc8d2c6658f612f73f6
Gustavo Gutierrez <gusguma@gmail.com>
2019-12-08 09:29:30 +0100

    Revert "create new test DecimalCollection class #13"
    This reverts commit 310550fe

--- src/test/java/es/upm/miw/iwvg/ecosystem/practicas/DecimalCollectionTest.java
+++ src/test/java/es/upm/miw/iwvg/ecosystem/practicas/DecimalCollectionTest.java
@@ -52,33 +52,4 @@ public class DecimalCollectionTest {
         assertThrows(ArithmeticException.class, () -> new DecimalCollection().higher());
     }
 
-    @Test
-    void testLower() {
-        assertEquals(-1.0, this.decimalCollection.lower(), 10e-5);
-    }
-
-    @Test
-    void testLowerArithmeticExceptionIfEmpty() {
-        assertThrows(ArithmeticException.class, () -> new DecimalCollection().lower());
-    }
-
-    @Test
-    void testAverage() {
-        assertEquals(1.5, this.decimalCollection.average(), 10e-5);
-    }
-
-    @Test
-    void testAverageArithmeticExceptionIfEmpty() {
-        assertThrows(ArithmeticException.class, () -> new DecimalCollection().average());
-    }
-
-    @Test
-    void testMultiply() {
-        assertEquals(-12.0, this.decimalCollection.multiply(), 10e-5);
-    }
-
-    @Test
-    void testMultiplyArithmeticExceptionIfEmpty() {
-        assertThrows(ArithmeticException.class, () -> new DecimalCollection().multiply());
-    }
 }
